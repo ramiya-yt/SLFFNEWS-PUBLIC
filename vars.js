aa4522043657b5bb446e66f306a5a024e5ffeb05
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2023-05-20 03:41:23 +0530

    Update vars.js

--- vars.js
+++ vars.js
@@ -1,6 +1,6 @@
 let x_083263e="Authorization error";
 	let ld_script="https://rawcdn.githack.com/ramiya-yt/SLFFNEWS-PUBLIC/94b819982a5a9888eb88d0adf1f923e44fd61d0a/tokencounter.js"
-	const x_086263e = ["https://www.slffnews.lk", "slffnews.lk", "slffnews.blogspot.com", "ramiyatestblog12.blogspot.com"];
+	const x_086263e = ["https://www.slffnews.lk", "slffnews.lk", "slffnews.blogspot.com", "www.slffnews.lk", "https://www.slffnews.lk/", "ramiyatestblog12.blogspot.com"];
 	let myvar3="Input a Number"
 	let myvar4="Enter Valid Number"
 	let myvar5=""
