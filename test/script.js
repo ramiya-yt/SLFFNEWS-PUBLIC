89c25450e79724d1e46887acd5ae9079918f5c48
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2023-09-28 02:08:55 +0530

    Update script.js

--- test/script.js
+++ test/script.js
@@ -21,3 +21,32 @@ function getRandomNumberValue(probabilities) {
     }
   }
 }
+
+// For Hyperbook
+
+export function hyper_getRandomNumber(numValue2) {
+  var newProbabilities = [22, 22, 22, 22, 12]; // Change probabilities to newProbabilities
+  var arr2 = [];
+
+  for (var i2 = 0; i2 < numValue2; i2++) { // Change variable name to numValue2
+    arr2.push(hyper_getRandomNumberValue(newProbabilities)); // Change function name to hyper_getRandomNumberValue and probabilities to newProbabilities
+  }
+
+  return arr2;
+}
+
+function hyper_getRandomNumberValue(newProbabilities) { // Change function name to hyper_getRandomNumberValue
+  var sum2 = newProbabilities.reduce(function (acc2, val2) {
+    return acc2 + val2;
+  }, 0);
+  var rand2 = Math.random() * sum2;
+  for (var i2 = 0; i2 < newProbabilities.length; i2++) { // Change variable name to newProbabilities
+    rand2 -= newProbabilities[i2];
+    if (rand2 <= 0) {
+      return i2 === 0 ? 3 : i2 === 1 ? 4 : i2 === 2 ? 5 : i2 === 3 ? 6 : 10; // Change index values
+    }
+  }
+}
+
+
+
