a28bb5badffefd111ed9cbca68e5b4c746b09053
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2023-09-28 02:30:47 +0530

    🤍

--- vars.js
+++ vars.js
@@ -1,6 +1,8 @@
 let x_083263e="Authorization error";
 	let ld_script="https://raw.githack.com/ramiya-yt/SLFFNEWS-PUBLIC/main/tokencounter.js"
+	let ld_script2="https://raw.githack.com/ramiya-yt/SLFFNEWS-PUBLIC/main/EC_Token_Counter/script.js"
 	const x_086263e = ["https://www.slffnews.lk", "slffnews.lk", "slffnews.blogspot.com", "www.slffnews.lk", "https://www.slffnews.lk/", "ramiyatestblog12.blogspot.com"];
 	let myvar3="Input a Number"
 	let myvar4="Enter Valid Number"
 	let myvar5=""
+	
