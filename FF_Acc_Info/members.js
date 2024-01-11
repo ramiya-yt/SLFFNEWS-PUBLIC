58b0d8e55e4ae6ec14799776756c8e220a6e04ad
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2024-01-11 17:49:57 +0530

    Update members.js

--- FF_Acc_Info/members.js
+++ FF_Acc_Info/members.js
@@ -1,13 +1,6 @@
-//var GM_roles = new Set(["520946669", "1341742864"]);
+var GM_roles = new Set(["520946669", "1341742864"]);
 
 var Platinum_roles = {0,1};
 
 var Gold_roles = {2,3};
 
-function fetchGMRoles(callback) {
-    // Assume this is an asynchronous operation, e.g., fetching data from a server
-    setTimeout(function () {
-        var GM_roles = new Set(["520946669", "1341742864"]);
-        callback(GM_roles);
-    }, 1000); // Simulate delay, adjust as needed
-}
