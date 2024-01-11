258667dd79d93abfc8215cc5c7fe8b6c00b1d35f
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2024-01-11 17:56:28 +0530

    Update members.js

--- FF_Acc_Info/members.js
+++ FF_Acc_Info/members.js
@@ -1,6 +1,5 @@
+// members.js
+
 var GM_roles = new Set(["520946669", "1341742864"]);
-
-var Platinum_roles = {0,1};
-
-var Gold_roles = {2,3};
-
+var Platinum_roles = new Set([0, 1]);
+var Gold_roles = new Set([2, 3]);
