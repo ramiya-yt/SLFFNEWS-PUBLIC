843be5156b777778e58a99b0789ae09f3e42c338
RamiYa YT <86957933+ramiya-yt@users.noreply.github.com>
2024-02-16 04:21:13 +0530

    Update members.js

--- FF_Acc_Info/members.js
+++ FF_Acc_Info/members.js
@@ -1,5 +1,5 @@
-// members.js
+var GOLD_roles = new Set(["520946669", "3"]);
 
-var GM_roles = new Set(["520946669", "1341742864"]);
-var Platinum_roles = new Set([0, 1]);
-var Gold_roles = new Set([2, 3]);
+var BLUE_roles = new Set(["2", "1341742864"]);
+
+var GREEN_roles = new Set(["2132773484", "1"]);
